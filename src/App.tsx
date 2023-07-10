4c7e4f8ccb7119cfa65e8b7937064f1148246a16
NQ-Thai <thainq200618.cdsg@saigontech.edu.vn>
2023-07-10 13:50:26 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Doisoatve from './Pages/Doisoatve';
+import Goidichvu from './Pages/Goidichvu';
 import Home from './Pages/Home/Home';
+import Quanlive from './Pages/Quanlive';
+import Trangchu from './Pages/Trangchu';
 
 function App() {
     return (
-        <div className="App">
-            <Home />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/trangchu" element={<Trangchu />} />
+                <Route path="/quanlive" element={<Quanlive />} />
+                <Route path="/doisoatve" element={<Doisoatve />} />
+                <Route path="/goidichvu" element={<Goidichvu />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -1,11 +1,17 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import logo from '../assets/images/insight logo.png';
-
 function Header() {
     return (
         <div>
             <header className="header">
-                <div className="">
-                    <img className="image-logo" src={logo} alt="" />
+                <img className="image-logo" src={logo} alt="" />
+                <div className="search-bar">
+                    {/* <form className="search-form d-flex align-items-center" action="#">
+                        <input type="text" placeholder="Search" />
+                        <button type="submit" title="Search">
+                            <i className="bi bi-search" />
+                        </button>
+                    </form> */}
                 </div>
             </header>
         </div>
