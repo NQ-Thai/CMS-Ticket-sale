c20637a3c1e97386df3deadd6e2317e9931c3832
NQ-Thai <thainq200618.cdsg@saigontech.edu.vn>
2023-07-17 14:13:52 +0700

    update

--- src/Pages/Quanlive/Quanlive.tsx
+++ src/Pages/Quanlive/Quanlive.tsx
@@ -1,4 +1,15 @@
-import { Button, Layout, Modal } from 'antd';
+import {
+    Button,
+    Checkbox,
+    Col,
+    DatePicker,
+    Layout,
+    Modal,
+    Radio,
+    RadioChangeEvent,
+    Row,
+} from 'antd';
+import { CheckboxValueType } from 'antd/es/checkbox/Group';
 import { Content } from 'antd/es/layout/layout';
 import { useState } from 'react';
 import { BsFunnel } from 'react-icons/bs';
@@ -8,7 +19,22 @@ import Search from './Search';
 import TableQuanLiVe from './Table';
 
 function Quanlive() {
+    // Modal
     const [modalOpen, setModalOpen] = useState(false);
+
+    // Radio
+    const [value, setValue] = useState(1);
+
+    const onChangeRadio = (e: RadioChangeEvent) => {
+        console.log('radio checked', e.target.value);
+        setValue(e.target.value);
+    };
+
+    //Check Box
+    const onChangeCheckBox = (checkedValues: CheckboxValueType[]) => {
+        console.log('checked = ', checkedValues);
+    };
+
     return (
         <div>
             <Layout>
@@ -24,7 +50,9 @@ function Quanlive() {
                             margin: '0 20px 1px 0',
                         }}
                     >
-                        <div className="content">Danh sách vé</div>
+                        <div style={{ font: 'Montserrat' }} className="content">
+                            Danh sách vé
+                        </div>
                         <div>
                             <Search />
                             <Button
@@ -76,16 +104,130 @@ function Quanlive() {
                     open={modalOpen}
                     onCancel={() => setModalOpen(false)}
                     title={
-                        <div className="modal-title" style={{ textAlign: 'center' }}>
-                            Lọc vé
+                        <div
+                            style={{
+                                textAlign: 'center',
+                                marginBottom: '26px',
+                            }}
+                        >
+                            <span className="modal-title" style={{ font: 'Montserrat' }}>
+                                Lọc vé
+                            </span>
                         </div>
                     }
                     footer={null}
                     centered
                 >
+                    <div style={{ marginBottom: '5px' }}>
+                        <span
+                            style={{
+                                font: 'Montserrat',
+                                paddingRight: '170px',
+                            }}
+                            className="modal-text"
+                        >
+                            Từ ngày
+                        </span>
+                        <span style={{ font: 'Montserrat' }} className="modal-text">
+                            Đến ngày
+                        </span>
+                    </div>
+                    {/* Lịch */}
                     <div>
-                        <span className="modal-text">Từ ngày</span>
-                        <span className="modal-text">Đến ngày</span>
+                        <div
+                            style={{
+                                paddingRight: '85px',
+                                display: 'inline',
+                            }}
+                        >
+                            <DatePicker
+                                className="custom-datepicker"
+                                style={{
+                                    width: '143px',
+                                    height: '40px',
+                                    borderColor: '#A5A8B1',
+                                }}
+                            />
+                        </div>
+                        <div
+                            style={{
+                                display: 'inline',
+                            }}
+                        >
+                            <DatePicker
+                                className="custom-datepicker"
+                                style={{
+                                    width: '143px',
+                                    height: '40px',
+                                    borderColor: '#A5A8B1',
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div style={{ marginTop: '20px' }}>
+                        <div>
+                            <span className="modal-text">Tình trạng sử dụng</span>
+                        </div>
+                        <div>
+                            <Radio.Group style={{ marginTop: '5px' }}>
+                                <Radio className="custom-radio" value={1}>
+                                    Tất cả
+                                </Radio>
+                                <Radio className="custom-radio" value={2}>
+                                    Đã sử dụng
+                                </Radio>
+                                <Radio className="custom-radio" value={3}>
+                                    Chưa sử dụng
+                                </Radio>
+                                <Radio className="custom-radio" value={4}>
+                                    Hết hạn
+                                </Radio>
+                            </Radio.Group>
+                        </div>
+                    </div>
+                    <div style={{ margin: '20px 0 40px 0' }}>
+                        <div>
+                            <span className="modal-text">Cổng Check-in</span>
+                        </div>
+                        <div>
+                            <Checkbox.Group
+                                style={{ width: '100%', marginTop: '8px' }}
+                                onChange={onChangeCheckBox}
+                            >
+                                <Row>
+                                    <Col span={8}>
+                                        <Checkbox className="custom-checkbox" value="A">
+                                            Tất cả
+                                        </Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox className="custom-checkbox" value="B">
+                                            Cổng 1
+                                        </Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox className="custom-checkbox" value="C">
+                                            Cổng 2
+                                        </Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox className="custom-checkbox" value="D">
+                                            Cổng 3
+                                        </Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox className="custom-checkbox" value="E">
+                                            Cổng 4
+                                        </Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox className="custom-checkbox" value="F">
+                                            Cổng 5
+                                        </Checkbox>
+                                    </Col>
+                                </Row>
+                            </Checkbox.Group>
+                        </div>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
                         <Button
@@ -93,6 +235,7 @@ function Quanlive() {
                             className="button"
                             type="primary"
                             style={{
+                                font: 'Montserrat',
                                 color: '#FF993C',
                                 borderColor: '#FF993C',
                                 height: '40px',
--- src/Pages/Trangchu.tsx
+++ src/Pages/Trangchu.tsx
@@ -1,11 +1,11 @@
-import { Layout } from 'antd';
+import { DatePicker, Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import Top from '../Component/Header';
 import NavBar from '../Component/NavBar';
 
 function Trangchu() {
     return (
-        <>
+        <div>
             <Layout>
                 <Top />
             </Layout>
@@ -15,12 +15,76 @@ function Trangchu() {
                     className="layout-content"
                     style={{
                         backgroundColor: '#FFFFFF',
+                        marginRight: '50px',
                     }}
                 >
-                    <Content className="content">Thống kê</Content>
+                    <Content
+                        style={{
+                            width: '100%',
+                        }}
+                    >
+                        <div className="content">Thống kê</div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <span
+                                className="chart-text"
+                                style={{
+                                    font: 'Montserrat',
+                                    alignSelf: 'flex-start',
+                                }}
+                            >
+                                Doanh thu
+                            </span>
+
+                            <DatePicker
+                                className="custom-datepicker"
+                                style={{
+                                    width: '155px',
+                                    height: '40px',
+                                    alignSelf: 'flex-end',
+                                    borderColor: '#A5A8B1',
+                                }}
+                            />
+                        </div>
+                        <div
+                            style={{
+                                padding: '90px',
+                            }}
+                        >
+                            {/* Chart line */}
+                        </div>
+                        <div className="total-div">
+                            <span className="total-text">Tổng danh thu theo tuần</span>
+                            <div>
+                                <span className="text-price">525.145.000</span>{' '}
+                                <span className="text-dong"> đồng</span>
+                            </div>
+                        </div>
+
+                        <div
+                            style={{
+                                marginTop: '30px',
+                            }}
+                            className="total-div"
+                        >
+                            <DatePicker
+                                className="custom-datepicker"
+                                style={{
+                                    width: '155px',
+                                    height: '40px',
+                                    alignSelf: 'flex-end',
+                                    borderColor: '#A5A8B1',
+                                }}
+                            />
+                        </div>
+                    </Content>
                 </Layout>
             </Layout>
-        </>
+        </div>
     );
 }
 
